dab49d88c8b372ecf9525bf91a62a5212eafd642
Maahi <Pradeeppatna8@gmail.com>
2026-08-18 23:55:28 +0530

    comitttttteee

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 // ============================================
 const allowedOrigins = [
   'https://ssfinworld-carhub.netlify.app',
-  'https://ssfinworld-carhub.netlify.app',
+  'https://ssfinworld.netlify.app/',
   'http://localhost:5173',
   'http://localhost:3000'
 ];
